f07c33e1ef7100b5a78d55cfeb8cd09a7d90732d
manelbaabou123 <47886652+manelbaabou123@users.noreply.github.com>
2023-05-04 13:31:06 +0100

    adds the Bookresource

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store()
+    {
+        // $data = request()->validate([
+        //     'title' => 'required',
+        //     'author' => 'required',
+        // ]);
+        // Book::create($data);
+
+        Book::create($this->validateRequest());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Book $book)
+    {
+        // $data = request()->validate([
+        //     'title' => 'required',
+        //     'author' => 'required',
+        // ]);
+        // $book->update($data);
+                //Refactor
+        $book->update($this->validateRequest());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+
+    public function validateRequest()
+    {
+       return request()->validate([
+            'title' => 'required',
+            'author' => 'required',
+        ]);
+    }
+}
