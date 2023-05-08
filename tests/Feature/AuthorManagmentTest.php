9dccb25a07a38e3c54f5be4e75bc93c71efbdccd
manelbaabou123 <47886652+manelbaabou123@users.noreply.github.com>
2023-05-08 14:08:28 +0100

    wip for authors resource

--- tests/Feature/AuthorManagmentTest.php
+++ tests/Feature/AuthorManagmentTest.php
@@ -12,18 +12,18 @@ class AuthorManagmentTest extends TestCase
 {
     use RefreshDatabase;
 
-       /** @test */
-       public function test_a_author_can_be_created()
-       {
-        //    $this->withoutExceptionHandling();
-           $this->withSession(['_token' => 'bzz'])->post('/authors/store', [
-               'name' => 'Author Name',
-               'dob' => '1988-05-05',
-               '_token' => 'bzz'
-           ]);
-           $author = Author::all();
-           $this->assertCount(1, $author);
-        //    $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+    /** @test */
+    public function test_a_author_can_be_created()
+    {
+        // $this->withoutExceptionHandling();
+        $this->withSession(['_token' => 'bzz'])->post('/authors/store', [
+            'name' => 'Author Name',
+            'dob' => '1988-05-05',
+            '_token' => 'bzz'
+        ]);
+        $author = Author::all();
+        $this->assertCount(1, $author);
+        // $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         // $this->assertEquals('1988-05-05', $author->first()->dob->format('Y/d/m'));
-       }
+    }
 }
